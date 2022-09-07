b74c621ac1e4a077301782d3385b22c3372cd7f1
reyfys <reyfelix82@gmail.com>
2022-09-07 19:46:37 +0700

    edit table for test

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,15 +1,15 @@
 class Table extends Component {
-    constructor(props) {
-        super(props);
-    }
-    state = {  }
-    render() { 
-        return ( 
-            <div>
-                <h1>Ini komponen Table</h1>
-            </div>
-         );
-    }
+  constructor(props) {
+    super(props);
+  }
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>Komponen Table</h1>
+      </div>
+    );
+  }
 }
- 
+
 export default Table;
