b454d897a4e986ac4bfa232ea54cc0efb9ac55ce
salsabilaathika-97 <salsathikar@gmail.com>
2022-09-13 19:52:29 +0800

    change card width and move to center

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -8,8 +8,12 @@ class Dashboard extends Component {
     state = {  }
     render() { 
         return ( 
-            <div>
-                <Card />
+            <div style = {{ height: 'auto', overflowX: 'hidden' }}>
+            <div className='row' style = {{ padding: 10 }}>
+                <div className='col' style={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap', marginTop: 8, marginBottom: 40, justifyContent: 'center'}}>
+                    <Card />
+                </div>
+            </div>
             </div>
          );
     }
