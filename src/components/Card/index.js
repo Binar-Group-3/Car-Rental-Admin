e5ee0d55fda83cd7f01caf0eead923dcd9299ff0
salsabilaathika-97 <salsathikar@gmail.com>
2022-09-14 20:22:08 +0800

    add ternary operator to show null name

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -21,8 +21,8 @@ const Card = () => {
                 dataUser.data.map(item => (
                     <div style={{ padding: 10 }}>
                             <div className="card" style={{padding: 25}}>
-                                {!!item.image ? <img className="rounded-t-lg" style = {{ maxHeight: 160, maxWidth: 270 }} src = {item.image} alt = "gambar-mobil"/> : <img className="rounded-t-lg" style = {{ maxHeight: 160, maxWidth: 270 }} src = {NoImage} alt = "no-image"/>}
-                                <div style={{fontFamily: 'arial', fontSize: 16, fontWeight: 700, fontStyle: 'normal', marginBottom: 8}}>{item.name}</div>    
+                                {!!item.image ? <img className="rounded-t-lg" style = {{ maxHeight: 160, maxWidth: 270 }} src = {item.image} alt = "gambar-mobil"/> : <img className="rounded-t-lg" style = {{ maxHeight: 160, maxWidth: 270 }} src = {NoImage} alt = "no-image"/>}   
+                                {!!item.name ? <div style={{fontFamily: 'arial', fontSize: 16, fontWeight: 700, fontStyle: 'normal', marginBottom: 8}}>{item.name}</div> : <div style={{fontFamily: 'arial', fontSize: 16, fontWeight: 700, fontStyle: 'normal', marginBottom: 8}}>Tidak tersedia</div>}
                                 <p>{item.price}</p>
                                 <p>{item.category}</p>
                                 <div class="inline-flex" role="group">
