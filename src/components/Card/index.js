d02b16fc077c3eac1c3bf0b0cf5897cb27d831d5
salsabilaathika-97 <salsathikar@gmail.com>
2022-09-14 15:43:17 +0800

    fix text position and error in format currency price

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -10,11 +10,6 @@ const Card = () => {
     useEffect(() => {
         dispatch(getData());
     }, []);
-
-    const formatCurrency = (number) => {
-        let fNumber = number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-        return "Rp. "+fNumber;
-    }
     
     return (
       <div>
@@ -23,10 +18,9 @@ const Card = () => {
                     <div style={{ padding: 10 }}>
                             <div className="card" style={{padding: 25}}>
                                 <img className="rounded-t-lg" src={item.image} style={{maxHeight: 160, maxWidth: 270}} alt="Gambar Mobil" />
-                                <p>Nama/Tipe Mobil</p>
-                                <div style={{fontFamily: 'arial', fontSize: 16, fontWeight: 700, fontStyle: 'normal', marginBottom: 8}}>{formatCurrency(item.price) + " / hari"}</div>
+                                <div style={{fontFamily: 'arial', fontSize: 16, fontWeight: 700, fontStyle: 'normal', marginBottom: 8}}>{item.name}</div>    
+                                <p>{item.price}</p>
                                 <p>{item.category}</p>
-                                <div className="tracking-tight text-gray-900 dark:text-white" style={{marginTop: 16}}>{item.name}</div>
                                 <div className="inline-flex justify-content-center py-2 px-3 text-sm font-bold text-center text-white bg-red-500 w-100 rounded-lg hover:bg-red-700 focus:ring-4 focus:outline-none">
                                     Delete
                                 </div >
