a40523a258d54e104fe866df1ec7c782ab58e304
salsabilaathika-97 <salsathikar@gmail.com>
2022-09-15 19:16:21 +0800

    add formatCurrency method for price with ternary operator

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -13,17 +13,20 @@ const Card = () => {
         dispatch(getData());
     }, []);
 
+    const formatCurrency = (number) => {
+        let fNumber = number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+        return !!fNumber ? "Rp. "+fNumber : "Harga tidak tersedia";
+    }
+
     return (
       <div>
-        
-           
            {
                 dataUser.data.map(item => (
                     <div style={{ padding: 10 }}>
                             <div className="card" style={{padding: 25}}>
                                 {!!item.image ? <img className="rounded-t-lg" style = {{ maxHeight: 160, maxWidth: 270 }} src = {item.image} alt = "gambar-mobil"/> : <img className="rounded-t-lg" style = {{ maxHeight: 160, maxWidth: 270 }} src = {NoImage} alt = "no-image"/>}   
                                 {!!item.name ? <div style={{fontFamily: 'arial', fontSize: 16, fontWeight: 700, fontStyle: 'normal', marginBottom: 8}}>{item.name}</div> : <div style={{fontFamily: 'arial', fontSize: 16, fontWeight: 700, fontStyle: 'normal', marginBottom: 8}}>Nama tidak tersedia</div>}
-                                {!!item.price ? <p>{item.price}</p> : <p>Harga tidak tersedia</p>}
+                                {!!item.price ? <p>{formatCurrency(item.price)}</p> : <p>Harga tidak tersedia</p>}
                                 {!!item.category ? <p>{item.category}</p> : <p>Kategori tidak tersedia</p>}
                                 <div class="inline-flex" role="group">
                                     <button type="button" class="text-red-700 hover:text-white border-2 border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Delete</button>
