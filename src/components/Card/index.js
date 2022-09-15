ccf0b14a8a38f4081b764586b9db6a4f75a8a9f6
salsabilaathika-97 <salsathikar@gmail.com>
2022-09-15 19:28:37 +0800

    add update status in card

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -28,6 +28,7 @@ const Card = () => {
                                 {!!item.name ? <div style={{fontFamily: 'arial', fontSize: 16, fontWeight: 700, fontStyle: 'normal', marginBottom: 8}}>{item.name}</div> : <div style={{fontFamily: 'arial', fontSize: 16, fontWeight: 700, fontStyle: 'normal', marginBottom: 8}}>Nama tidak tersedia</div>}
                                 {!!item.price ? <p>{formatCurrency(item.price)}</p> : <p>Harga tidak tersedia</p>}
                                 {!!item.category ? <p>{item.category}</p> : <p>Kategori tidak tersedia</p>}
+                                {!!item.updatedAt ? <p style = {{ fontFamily: 'arial', fontSize: 12 }}>Updated at {item.updatedAt}</p> : <p>Tidak ada update baru</p>}
                                 <div class="inline-flex" role="group">
                                     <button type="button" class="text-red-700 hover:text-white border-2 border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Delete</button>
                                     <Link to = {`/edit/${item.id}`}>
