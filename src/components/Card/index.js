1eb33910ef84a1eb3ef5951b41858a7aa67dcc68
salsabilaathika-97 <salsathikar@gmail.com>
2022-09-15 20:42:21 +0800

    change card style to have fixed size

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -22,8 +22,8 @@ const Card = () => {
       <div className="grid gap-4 grid-cols-3 grid-rows-3">
            {
                 dataUser.data.map(item => (
-                    <div style={{ padding: 10 }}>
-                            <div className="card" style={{padding: 25}}>
+                    <div className="inline-flex p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md" style={{ padding: 10 }}>
+                            <div style={{padding: 25}}>
                                 {!!item.image ? <img className="rounded-t-lg" style = {{ maxHeight: 160, maxWidth: 270 }} src = {item.image} alt = "gambar-mobil"/> : <img className="rounded-t-lg" style = {{ maxHeight: 160, maxWidth: 270 }} src = {NoImage} alt = "no-image"/>}   
                                 {!!item.name ? <div style={{fontFamily: 'arial', fontSize: 16, fontStyle: 'normal', marginBottom: 8}}>{item.name}</div> : <div style={{fontFamily: 'arial', fontSize: 16, fontWeight: 700, fontStyle: 'normal', marginBottom: 8}}>Nama tidak tersedia</div>}
                                 {!!item.price ? <p style = {{ fontFamily: 'arial', fontWeight: 700 }}>{formatCurrency(item.price)} / hari</p> : <p style = {{ fontFamily: 'arial' }}>Harga tidak tersedia</p>}
