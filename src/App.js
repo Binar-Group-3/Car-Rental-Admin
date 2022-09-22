255d797cfa65640fea3688ee3f194720fdf1eaea
salsabilaathika-97 <salsathikar@gmail.com>
2022-09-22 19:22:38 +0800

    add list car to router in app js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Dashboard from './pages/Dashboard';
 import {Routes, Route, BrowserRouter} from "react-router-dom";
 import EditPage from './pages/EditPage';
+import ListCar from './pages/ListCar';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
           <Routes>
             <Route path = "/" element = {<Dashboard />} />
             <Route path = "/edit/:id" element= {<EditPage />} />
+            <Route path = "/listcar" element={<ListCar />} />
           </Routes>
         </BrowserRouter>
     </div>
