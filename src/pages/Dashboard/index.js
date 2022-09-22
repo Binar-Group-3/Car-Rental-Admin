0cf5a4c0857295bbc3dbb5d36e9b9cfe8c9a59c6
salsabilaathika-97 <salsathikar@gmail.com>
2022-09-22 19:21:58 +0800

    revert dashboard page to original

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,6 +1,3 @@
-import Card from "../../components/Card";
-import React, {Component} from "react";
-
 class Dashboard extends Component {
     constructor(props) {
         super(props);
@@ -8,12 +5,8 @@ class Dashboard extends Component {
     state = {  }
     render() { 
         return ( 
-            <div style = {{ height: 'auto', overflowX: 'hidden' }}>
-            <div className='row' style = {{ padding: 10 }}>
-                <div className='col' style={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap', marginTop: 8, marginBottom: 40, justifyContent: 'center'}}>
-                    <Card />
-                </div>
-            </div>
+            <div>
+                <h1>Ini halaman dashboard</h1>
             </div>
          );
     }
