7f27c22a9d562799e1edef6b1cb06eec6ad0151a
Miechael888 <miechaeltakumansang@gmail.com>
2022-09-25 23:01:09 +0700

    Creating Error-Login_card

---
+++ src/components/Login/ErrorLoginCard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ErrorLoginCard = () => {
+  return (
+    <div>
+      <p>Masukkan username dan password yang benar. Perhatikan penggunaan huruf kapital.</p>
+    </div>
+  );
+};
+
+export default ErrorLoginCard;
