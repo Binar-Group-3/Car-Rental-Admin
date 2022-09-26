4eb56f0e15feaff5b69ab04b50ba02a0a25b9b39
Miechael888 <miechaeltakumansang@gmail.com>
2022-09-26 09:06:38 +0700

    adding Login-pages

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,14 +1,40 @@
-class Login extends Component {
-    constructor(props) {
-        super(props);
-    }
-    state = {  }
-    render() { 
-        return ( 
-        <div>
-            <h1>Ini halaman login</h1>
-        </div> );
-    }
-}
- 
-export default Login;
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate, useLocation } from "react-router-dom";
+
+import Login from "../components/Login";
+import { handleLogin } from "../redux/actions/authAction";
+
+const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errMsg, setErrMsg] = useState(false);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const user = localStorage.getItem("token");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const payload = {
+      email,
+      password,
+    };
+    dispatch(handleLogin(payload, setErrMsg, navigate));
+  };
+
+  const props = {
+    email,
+    password,
+    setEmail,
+    setPassword,
+    handleSubmit,
+    errMsg,
+  };
+
+  return <div>{<Login {...props} />}</div>;
+};
+
+export default LoginPage;
