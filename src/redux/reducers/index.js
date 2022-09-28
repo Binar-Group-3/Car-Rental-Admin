853ba187cde0c9f4a4caf11d39559b4842851421
salsabilaathika-97 <salsathikar@gmail.com>
2022-09-28 15:12:02 +0800

    setup redux for modal delete

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,13 @@
 import {combineReducers} from "redux";
+import carReducer from "./carReducer";
 import dataReducer from "./dataReducer";
 import orderReducer from "./orderReducer";
+
 
 const rootReducer = combineReducers({
     dataUser: dataReducer,
     orderReducer,
+    car: carReducer,
 })
 
 export default rootReducer;
