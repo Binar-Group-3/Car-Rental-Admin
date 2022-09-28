0d6ea1fdcf2d874c45ff2f4a2cc919caa3ee70d1
salsabilaathika-97 <salsathikar@gmail.com>
2022-09-28 14:26:23 +0800

    fix module error by change import source

--- src/pages/LoginPage/index.js
+++ src/pages/LoginPage/index.js
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate, useLocation } from "react-router-dom";
-import Login from "../components/Login";
-import { handleLogin } from "../redux/actions/authAction";
+import Login from "../../components/Login";
+import { handleLogin } from "../../redux/actions/authAction";
 
 const LoginPage = () => {
   const [email, setEmail] = useState("");
