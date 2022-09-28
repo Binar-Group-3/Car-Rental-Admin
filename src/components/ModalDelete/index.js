aa95a228431cae0062a628e9281b23ffed04c0d2
salsabilaathika-97 <salsathikar@gmail.com>
2022-09-28 15:08:18 +0800

    create modal delete and import to card component

--- src/components/ModalDelete/index.js
+++ src/components/ModalDelete/index.js
@@ -1,8 +1,40 @@
-const ModalDelete = () => {
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import { ModalImg } from '../../assets';
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
+const ModalDelete = (props) => {
+    const { handleClose, open, item} = props
     return (
-        <div>
-            <h1>Ini component modal delete</h1>
-        </div>
+        <Modal 
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+            >
+            <Box sx = {style}>
+                <img src = {ModalImg} alt = "car" />
+                    <Typography id="modal-modal-title" variant="h3" component="h2">
+                        Menghapus data mobil
+                    </Typography>
+                    <p>Setelah dihapus, data mobil tidak dapat dikembalikan, Yakin ingin
+                    menghapus? {item.name}</p>
+                <button>Ya</button>
+                <button>Tidak</button>
+            </Box>
+        </Modal>
     )
 }
 
