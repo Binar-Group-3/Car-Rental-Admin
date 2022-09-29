7f9ad1c10e5140907d2dd4a1bed75741f86f459d
tribagus <tri.bagus21@gmail.com>
2022-09-29 11:26:28 +0700

    setup edit car component

---
+++ src/components/EditCarComp/index.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react"
+import { useDispatch } from "react-redux"
+import { useParams } from "react-router-dom"
+
+const EditCarComp = () => {
+  return (
+    <section className="edit-car-comp">
+      <main className="edit-car-comp"></main>
+    </section>
+  )
+}
