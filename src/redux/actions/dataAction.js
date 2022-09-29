6872f372952d0257243ead6f4ae1cfd259ee61d0
tribagus <tri.bagus21@gmail.com>
2022-09-29 11:35:22 +0700

    add action for individual car

--- src/redux/actions/dataAction.js
+++ src/redux/actions/dataAction.js
@@ -1,27 +1,39 @@
-import axios from 'axios';
-import TYPES from "../types";
+import axios from "axios"
+import TYPES from "../types"
 
 export const getData = () => (dispatch) => {
-    axios
-      .get("https://bootcamp-rent-car.herokuapp.com/admin/car")
-      .then((res) => {
-        dispatch({
-            type: TYPES.FETCH_DATA,
-            payload: res.data,
-        })
+  axios
+    .get("https://bootcamp-rent-car.herokuapp.com/admin/car")
+    .then((res) => {
+      dispatch({
+        type: TYPES.FETCH_DATA,
+        payload: res.data,
       })
-      .catch((err) => console.log(err));
+    })
+    .catch((err) => console.log(err))
 }
 
 export const getMockData = () => (dispatch) => {
-    axios
+  axios
     .get("http://localhost:3001/admin/car")
     .then((res) => {
       dispatch({
-          type: TYPES.FETCH_DATA,
-          payload: res.data,
+        type: TYPES.FETCH_DATA,
+        payload: res.data,
       })
-      console.log(res.data);
+      console.log(res.data)
     })
-    .catch((err) => console.log(err));
+    .catch((err) => console.log(err))
+}
+
+export const handleCar = (id) => (dispatch) => {
+  axios
+    .get(`https://bootcamp-rent-car.herokuapp.com/admin/car/${id}`)
+    .then((res) => {
+      dispatch({
+        type: TYPES.GET_CAR,
+        payload: res.data,
+      })
+    })
+    .catch((err) => console.log(err.message))
 }
