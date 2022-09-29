f4dabd65e3e4b65dd75eb40e26e447072f7e7f28
tribagus <tri.bagus21@gmail.com>
2022-09-29 14:47:52 +0700

    add edit breadcrumb

---
+++ src/components/EditCarComp/EditHeader/EditBreadcrumb.js
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom"
+
+const EditBreadcrumb = () => {
+  return (
+    <section className="edit-breadcrumb">
+      <div className="edit-breadcrumb-container ">
+        <p>Cars</p>
+        <i className="uil uil-angle-right-b"></i>
+        <div className="link">
+          <Link to="/cars">List Car</Link>
+        </div>
+        <i className="uil uil-angle-right-b"></i>
+        <div className="link">
+          <Link to="#">Edit Car</Link>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default EditBreadcrumb
