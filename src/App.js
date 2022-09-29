cd3f5c6f099c7f2e5d56ab511e592c895aa3c5a2
tribagus <tri.bagus21@gmail.com>
2022-09-29 11:09:36 +0700

    update login page path routes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import Dashboard from './pages/Dashboard';
-import {Routes, Route, BrowserRouter} from "react-router-dom";
-import EditPage from './pages/EditPage';
-import ListCar from './pages/ListCar';
-import LoginPage from "./pages/LoginPage";
+import Dashboard from "./pages/Dashboard"
+import { Routes, Route, Navigate } from "react-router-dom"
+import EditPage from "./pages/EditPage"
+import ListCar from "./pages/ListCar"
+import LoginPage from "./pages/LoginPage"
 
 function App() {
   return (
     <div>
-        <BrowserRouter>
-          <Routes>
-            <Route path = "/" element = {<Dashboard />} />
-            <Route path = "/edit/:id" element= {<EditPage />} />
-            <Route path = "/listcar" element={<ListCar />} />
-            <Route path="/login-page" element={<LoginPage />} />
-          </Routes>
-        </BrowserRouter>
+      <Routes>
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="cars" element={<ListCar />} />
+        <Route path="cars/edit-car/:id" element={<EditPage />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="*" element={<Navigate to="dashboard" replace />} />
+      </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
