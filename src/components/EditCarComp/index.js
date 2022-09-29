bfc5f85b0baf98a917e5edf9cd8b8b34cf2805f6
tribagus <tri.bagus21@gmail.com>
2022-09-29 11:32:44 +0700

    add state and hook setup

--- src/components/EditCarComp/index.js
+++ src/components/EditCarComp/index.js
@@ -2,7 +2,35 @@ import React, { useState, useEffect } from "react"
 import { useDispatch } from "react-redux"
 import { useParams } from "react-router-dom"
 
+// func
+
 const EditCarComp = () => {
+  // state
+  const [carName, setCarName] = useState("")
+  const [carPrice, setCarPrice] = useState("")
+  const [carImage, setCarImage] = useState("")
+  const [carCategory, setCarCategory] = useState("")
+
+  // hook
+  const dispatch = useDispatch()
+  const param = useParams()
+
+  useEffect(() => {
+    const id = param.id
+    // dispatch(handleCar(id))
+  }, [dispatch, param.id])
+
+  const props = {
+    setCarName,
+    setCarPrice,
+    setCarImage,
+    setCarCategory,
+    carName,
+    carPrice,
+    carImage,
+    carCategory,
+  }
+
   return (
     <section className="edit-car-comp">
       <main className="edit-car-comp"></main>
