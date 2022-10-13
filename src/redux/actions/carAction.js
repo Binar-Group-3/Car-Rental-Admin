52457368164c279a938f6ad6879c8b2bc5b30bb6
salsabilaathika-97 <salsathikar@gmail.com>
2022-10-13 22:18:50 +0800

    replace api in data action and car action

--- src/redux/actions/carAction.js
+++ src/redux/actions/carAction.js
@@ -3,14 +3,20 @@ import TYPES from "../types"
 
 export const handleCar = () => (dispatch) => {
   axios
-  .get("https://bootcamp-rent-car.herokuapp.com/admin/car")
+  .get("https://bootcamp-rent-cars.herokuapp.com/admin/v2/car",{
+    headers: {
+      "Content-Type": "Application/json",
+      "accept": "Application/json",
+      "access_token": 
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNhbHNhQHNhbHNhLmNvbSIsInJvbGUiOiJBZG1pbiIsImlhdCI6MTY2NTY3MDM3N30.iR-2vyAeAZX11u7AOKXCgNJYey-_eg58E8zZVgu4Z7Y"
+    }})
   .then((res) => {
     dispatch({
-      type: TYPES.GET_CARS,
+      type: TYPES.FETCH_DATA,
       payload: res.data,
     })
   })
-  .catch((err) => console.log(err.message))
+  .catch((err) => console.log(err))
   }
 export const getCar = (id) => (dispatch) => {
   axios
