779159d9d84ad756ea149a7b792b247690e9e458
reyfys <reyfelix82@gmail.com>
2022-10-15 00:03:17 +0700

    change API

--- src/redux/actions/dataTableAction.js
+++ src/redux/actions/dataTableAction.js
@@ -3,11 +3,18 @@ import axios from "axios";
 
 export const getDataOrder = () => (dispatch) => {
   axios
-    .get("https://bootcamp-rent-car.herokuapp.com/admin/order")
+    .get("https://bootcamp-rent-cars.herokuapp.com/admin/v2/order", {
+      headers: {
+        "Content-Type": "Application/json",
+        "accept": "Application/json",
+        "access_token":
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluMTIzQGdtYWlsLmNvbSIsInJvbGUiOiJBZG1pbiIsImlhdCI6MTY2NTY3MDcxM30.pu-ESe4QOfLirSYImxk-Fncw-vbr6PqbpQ4fJZ6bZhE",
+      },
+    })
     .then((res) => {
       dispatch({
         type: TYPES.GET_TABLE_DATA_ORDER,
-        payload: res.data,
+        payload: res.data.orders,
       });
     })
     .catch((err) => console.log(err));
