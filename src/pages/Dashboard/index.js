a43f0b40611141506b73b2e39596400567915701
amalrivel <amalrivel@gmail.com>
2022-10-16 20:43:39 +0700

    adding sidebar and navbar at dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,22 +1,38 @@
 import React from "react";
 import rectangle from "../../assets/rectangle.png";
-import "./dashboard.css";
+import "./dashboard.scss";
+import Navbar from "../../components/Navbar";
+import Sidebar from "../../components/Sidebar";
 const TableComp = React.lazy(() =>
   import("../../components/Table/TableComp/tableComp")
 );
 
 const Dashboard = () => {
+  const [sidebar, setSidebar] = React.useState(false);
+  const handleSidebar = () => {
+    setSidebar(!sidebar);
+  };
   return (
-    <div className="container">
-      <h1>Dashboard</h1>
-      <div className="list_order">
-        <img src={rectangle} />
-        <h2>List Order</h2>
+    <React.Fragment>
+      <Navbar handleSidebar={handleSidebar} sidebar={sidebar} />
+      <Sidebar sidebar={sidebar} />
+      <div
+        className={`container-dasboard__ ${
+          sidebar === true ? "container-dasboard__on" : "container-dasboard__off"
+        }`}
+      >
+        <div className="container">
+          <h1>Dashboard</h1>
+          <div className="list_order">
+            <img src={rectangle} />
+            <h2>List Order</h2>
+          </div>
+          <div className="table">
+            <TableComp />
+          </div>
+        </div>
       </div>
-      <div className="table">
-        <TableComp />
-      </div>
-    </div>
+    </React.Fragment>
   );
 };
 
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,12 +3,14 @@ import carReducer from "./carReducer";
 import orderReducer from "./orderReducer";
 import authReducer from "./authReducer";
 import dataReducer from "./dataReducer"
+import navbarReducer from "./navbarReducer"
 
 const rootReducer = combineReducers({
     orderReducer,
     car: carReducer,
     authReducer,
     dataUser: dataReducer,
+    navbarReducer
 })
 
 export default rootReducer
