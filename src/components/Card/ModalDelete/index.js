1b7adf9248b69237f6a02d86d47ed07edc144418
salsabilaathika-97 <salsathikar@gmail.com>
2022-10-17 11:09:02 +0800

    create base css for modal delete

--- src/components/Card/ModalDelete/index.js
+++ src/components/Card/ModalDelete/index.js
@@ -1,21 +1,6 @@
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
 import { ModalImg } from '../../../assets';
 import axios from "axios";
 import swal from "sweetalert";
-
-const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
-  };
 
 const ModalDelete = (props) => {
     const { handleOpen, handleClose, carId, open} = props
@@ -47,23 +32,25 @@ const ModalDelete = (props) => {
     }
 
     return (
-        <Modal 
-            open={open}
-            onClose={handleClose}
-            aria-labelledby="modal-modal-title"
-            aria-describedby="modal-modal-description"
-            >
-            <Box sx = {style}>
-                <img src = {ModalImg} alt = "car" />
-                    <Typography id="modal-modal-title" variant="h3" component="h2">
-                        Menghapus data mobil
-                    </Typography>
-                    <p>Setelah dihapus, data mobil tidak dapat dikembalikan, Yakin ingin
-                    menghapus?</p>
-                <button onClick={() => handleDelete(carId)}>Ya</button>
-                <button onClick={handleOpen}>Tidak</button>
-            </Box>
-        </Modal>
+        <div className="modal-delete__backdrop">
+            <div className="modal">
+                <div className="modal-content">
+                    <img src = {ModalImg} alt="car"/>
+                    <h3>Menghapus Data Mobil?</h3>
+                    <p>
+                    Setelah dihapus, data mobil tidak dapat dikembalikan, Yakin ingin menghapus?
+                    </p>
+                <div className="modal-button">
+                    <button onClick={() => handleDelete(carId)}>
+                        Ya
+                    </button>
+                    <button onClick={handleOpen}>
+                        Tidak
+                    </button>
+                </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
