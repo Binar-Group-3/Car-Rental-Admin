38842f0f43947694fe19a2492578529da9416fbd
reyfys <reyfelix82@gmail.com>
2022-10-18 21:27:40 +0700

    change css to sass

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,24 +1,26 @@
 import React from "react";
 import rectangle from "../../assets/rectangle.png";
 import ChartComp from "../../components/Chart/chartComp";
-import "./dashboard.css";
+import "./dashboard.scss";
 const TableComp = React.lazy(() =>
   import("../../components/Table/TableComp/tableComp")
 );
 
 const Dashboard = () => {
   return (
-    <div className="container-dashboard">
-      <h1>Dashboard</h1>
-      <div className="list_order">
-        <img src={rectangle} />
-        <h2>List Order</h2>
-      </div>
-      <div>
-        <ChartComp />
-      </div>
-      <div className="table-component">
-        <TableComp />
+    <div className="dashboard_style">
+      <div className="container-dashboard">
+        <h1>Dashboard</h1>
+        <div className="list_order">
+          <img src={rectangle} />
+          <h2>List Order</h2>
+        </div>
+        <div>
+          <ChartComp />
+        </div>
+        <div className="table-component">
+          <TableComp />
+        </div>
       </div>
     </div>
   );
