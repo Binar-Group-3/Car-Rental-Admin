ef047162a4b6f218d98a25626e18bee121e3c058
reyfys <reyfelix82@gmail.com>
2022-10-18 20:59:45 +0700

    fetch data to chart

--- src/components/Chart/chartComp.js
+++ src/components/Chart/chartComp.js
@@ -1,7 +1,40 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getDataChart } from "../../redux/actions/dataChartAction";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+} from "chart.js";
+import moment from "moment";
 import BarChart from "./barChart";
 
 const ChartComp = () => {
+  const { chartData } = useSelector((state) => state.chartReducer)
+  const [value, setValue] = useState("");
+  const dispatch = useDispatch();
+
+  const getData = (value) => {
+    dispatch(getDataChart(value));
+  };
+
+  useEffect(() => {
+    getData(value);
+  }, []);
+
+  // FOR GET MONTH ONLY
+  const monthData = chartData.map((item) => moment(item, "D MMM").format("MMM"))
+  const monthOnly = monthData.filter((element, index) => {
+    return monthData.indexOf(element) === index;
+  });
+
+  // FOR HANDLE CLICK
+  const handleClick = () => {
+    getData(value)
+  }
+
   return (
     <div>
       <BarChart />
