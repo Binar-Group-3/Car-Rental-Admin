b1fc6aa8d47e74b684cd32ec117312ee75ef838f
salsabilaathika-97 <salsathikar@gmail.com>
2022-10-21 22:11:45 +0800

    add return in list header component

--- src/components/ListHeader/index.js
+++ src/components/ListHeader/index.js
@@ -3,10 +3,12 @@ import CarFilter from "./CarFilter";
 import CarListTitle from "./CarListTitle";
 
 const ListHeader = () => {
-    <>
-        <CarListTitle />
-        <CarFilter />
-    </>
+    return (
+        <>
+            <CarListTitle />
+            <CarFilter />
+        </>
+    )
 }
 
 export default ListHeader;
