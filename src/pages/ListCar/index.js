49c94b6a3d43963648761fd2e5abc7c793a07b44
salsabilaathika-97 <salsathikar@gmail.com>
2022-10-21 11:57:25 +0800

    fix modal delete error by add handle delete show in card

--- src/pages/ListCar/index.js
+++ src/pages/ListCar/index.js
@@ -28,7 +28,7 @@ const ListCar = () => {
                 <div className='row' style = {{ padding: 10 }}>
                     {dataUser.data.map((item) => (
                         <div className='col-md-3' key={item.id} style={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap', marginTop: 8, marginBottom: 40, justifyContent: 'center'}}>
-                            <Card item={item} />
+                            <Card item={item} handleDelete={modalDeleteShow} />
                         </div>
                     ))}
                 </div>
