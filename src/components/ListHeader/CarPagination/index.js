fc8be08c2d09848097304b080704ca0252cdd327
tribagus <tri.bagus21@gmail.com>
2022-10-22 15:59:28 +0700

    add function to car pagination and change page to 8 show only

--- src/components/ListHeader/CarPagination/index.js
+++ src/components/ListHeader/CarPagination/index.js
@@ -1,14 +1,22 @@
 import React from "react"
 import "./CarPagination.css"
 
-const CarPagination = () => {
+const CarPagination = ({ setPage, page, data }) => {
+  // func
+  const nextPage = () => {
+    setPage((prev) => prev + 1)
+  }
+  const prevPage = () => {
+    setPage((prev) => Math.max(prev - 1, 0))
+  }
+
   return (
     <nav className="car-pagination-container-only">
-      <button>
+      <button onClick={prevPage} disabled={page === 1}>
         <span>&lt;&lt;</span>
       </button>
-      <p>page 1 of 1</p>
-      <button>
+      <p>{`Page ${page} of ${data?.pageCount}`}</p>
+      <button onClick={nextPage} disabled={page === data?.pageCount}>
         <span>&gt;&gt;</span>
       </button>
     </nav>
