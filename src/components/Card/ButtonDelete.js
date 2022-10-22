73f48b228c75f5f3f80ca9def46150a8646461fe
tribagus <tri.bagus21@gmail.com>
2022-10-22 14:57:32 +0700

    install react query

--- src/components/Card/ButtonDelete.js
+++ src/components/Card/ButtonDelete.js
@@ -1,14 +1,21 @@
-import {Button} from "react-bootstrap";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faTrashAlt } from "@fortawesome/free-regular-svg-icons"
+import { Button } from "react-bootstrap"
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+// import { faTrashAlt } from "@fortawesome/free-regular-svg-icons"
 
-const ButtonDelete = ({carId, onClick}) => {
-    
-    return (
-            <Button onClick={() => { onClick(carId) }} variant="outline-danger" size="md" className="w-100">
-                <FontAwesomeIcon icon={faTrashAlt} /> Delete
-            </Button>
-    )
+const ButtonDelete = ({ carId, onClick }) => {
+  return (
+    <Button
+      onClick={() => {
+        onClick(carId)
+      }}
+      variant="outline-danger"
+      size="md"
+      className="w-100"
+    >
+      {/* <FontAwesomeIcon icon={faTrashAlt} /> */}
+      Delete
+    </Button>
+  )
 }
 
-export default ButtonDelete;
+export default ButtonDelete
