5a7b84e29567bf8683cb5b855b11dd714b9f8216
salsabilaathika-97 <salsathikar@gmail.com>
2022-10-22 19:03:37 +0800

    change add new car button style

--- src/components/ListHeader/CarListTitle.js
+++ src/components/ListHeader/CarListTitle.js
@@ -1,6 +1,8 @@
 import React from "react";
 import {Button} from "react-bootstrap";
 import CarBreadcrumb from "./CarBreadcrumb";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 const CarListTitle = () => {
     return (
@@ -10,8 +12,8 @@ const CarListTitle = () => {
                 <h1 className='mx-3 my-3'>List Car</h1>
             </div>
             <div className="col-6">
-                <Button variant = "primary" size="md" className="position-absolute top-20 end-0 mx-3 my-4">
-                    Add New Car
+                <Button variant = "default" size="md" style = {{ color: "white", background: "#0D28A6" }} className="position-absolute top-20 end-0 mx-3 my-4">
+                <FontAwesomeIcon icon={faPlus} style={{ marginRight: 10 }} />Add New Car
                 </Button>
             </div>
         </div>
