d9093501b9eb7fa18de6c543ba0696c4c55c5ac7
salsabilaathika-97 <salsathikar@gmail.com>
2022-10-23 21:46:55 +0800

    create header component for add car page

---
+++ src/components/AddCarComp/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import AddHeader from "./AddHeader";
+import "./index.css"
+
+const AddCarComp = () => {
+    return (
+        <section className="add-car-comp">
+            <main className="add-car-comp__container">
+                <AddHeader />
+            </main>
+        </section>
+    )
+}
+
+export default AddCarComp;
--- src/pages/AddPage/index.js
+++ src/pages/AddPage/index.js
@@ -1,8 +1,16 @@
 import React from "react";
+import AddCarComp from "../../components/AddCarComp";
 
 const AddPage = () => {
     return (
         <>
+            <main>
+                {/* <Header /> */}
+                {/* <Sidebar /> */}
+                <section className="add-car-page">
+                <AddCarComp />
+                </section>
+            </main>
         </>
     )
 }
