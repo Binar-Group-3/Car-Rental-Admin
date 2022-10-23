3b1676cbb5d320855a5bea9b44bbdf81d3f724c1
salsabilaathika-97 <salsathikar@gmail.com>
2022-10-23 21:22:38 +0800

    create empty add page and button link to add page

--- src/components/ListHeader/CarListTitle.js
+++ src/components/ListHeader/CarListTitle.js
@@ -3,6 +3,7 @@ import {Button} from "react-bootstrap";
 import CarBreadcrumb from "./CarBreadcrumb";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom"
 
 const CarListTitle = () => {
     return (
@@ -12,9 +13,11 @@ const CarListTitle = () => {
                 <h1 className='mx-3 my-3'>List Car</h1>
             </div>
             <div className="col-6">
-                <Button variant = "default" size="md" style = {{ color: "white", background: "#0D28A6" }} className="position-absolute top-20 end-0 mx-3 my-4">
-                <FontAwesomeIcon icon={faPlus} style={{ marginRight: 10 }} />Add New Car
-                </Button>
+                <Link to = {`/dashboard/cars/add`}>
+                    <Button variant = "default" size="md" style = {{ color: "white", background: "#0D28A6" }} className="position-absolute top-20 end-0 mx-3 my-4">
+                        <FontAwesomeIcon icon={faPlus} style={{ marginRight: 10 }} />Add New Car
+                    </Button>
+                </Link>
             </div>
         </div>
     )
